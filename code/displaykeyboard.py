e86caf50c879d68298568b31db4799c4cb3c145a
TseManFung <163531908+TseManFung@users.noreply.github.com>
2024-10-13 17:39:01 +0800

    Refactor displaykeyboard.py and main.py

--- code/displaykeyboard.py
+++ code/displaykeyboard.py
@@ -1,53 +1,72 @@
 # encoding: utf-8
 from math import sqrt
 
-rows_num_keys = [0, 10, 19, 26]
 
-default_keyboard = {
-    'Q': 0, 'W': 1, 'E': 2, 'R': 3, 'T': 4, 'Y': 5, 'U': 6, 'I': 7, 'O': 8, 'P': 9,
-    'A': 10, 'S': 11, 'D': 12, 'F': 13, 'G': 14, 'H': 15, 'J': 16, 'K': 17, 'L': 18,
-    'Z': 19, 'X': 20, 'C': 21, 'V': 22, 'B': 23, 'N': 24, 'M': 25
-}
+class Keyboard():
+    def __init__(self, finger_group: list = None, finger_default_key=None, keyboard: dict = None):
+        if finger_group:
+            self.finger_group = finger_group
+        else:
+            self.finger_group = [
+                0, 1, 2, 3, 3, 4, 4, 5, 6, 7,
+                0, 1, 2, 3, 3, 4, 4, 5, 6,
+                0, 1, 2, 3, 3, 4, 4
+            ]
+        if finger_default_key:
+            self.finger_key = finger_default_key
+        else:
+            self.finger_key = [None]*8
+        if keyboard:
+            self.keyboard = keyboard
+        else:
+            self.keyboard = {
+                'Q': 0, 'W': 1, 'E': 2, 'R': 3, 'T': 4, 'Y': 5, 'U': 6, 'I': 7, 'O': 8, 'P': 9,
+                'A': 10, 'S': 11, 'D': 12, 'F': 13, 'G': 14, 'H': 15, 'J': 16, 'K': 17, 'L': 18,
+                'Z': 19, 'X': 20, 'C': 21, 'V': 22, 'B': 23, 'N': 24, 'M': 25
+            }
 
-def init_keyboard():
-    return default_keyboard.copy()
-
-def display_keyboard(keyboard):
-    key_list = list(keyboard.keys())
-    pos_list = list(keyboard.values())
-    row = 0
-    for row_num in range(3):
-        print(" "*(row+row-1), end="")
-        for key_pos in range(rows_num_keys[row], rows_num_keys[row+1]):
-            print("   "+key_list[pos_list.index(key_pos)], end="")
-        print("")
-        row += 1
-
-
-def get_y(w_pos):
-    return (w_pos > 18) + (w_pos > 9)
-
-
-def distance(keyboard, w1, w2):
+    rows_num_keys = [0, 10, 19, 26]
     row_start = [0, 1, 3]
-    w1_y = get_y(keyboard[w1])
-    w2_y = get_y(keyboard[w2])
-    w1_x = 4*(keyboard[w1] - rows_num_keys[w1_y]) + row_start[w1_y]
-    w2_x = 4*(keyboard[w2] - rows_num_keys[w2_y]) + row_start[w2_y]
-    return sqrt((w1_x - w2_x)**2 + (4*w1_y - 4*w2_y)**2)
 
+    def display_keyboard(self):
+        key_list = list(self.keyboard.keys())
+        pos_list = list(self.keyboard.values())
+        row = 0
+        for row_num in range(3):
+            print(" "*(row+row-1), end="")
+            for key_pos in range(self.rows_num_keys[row], self.rows_num_keys[row+1]):
+                print("   "+key_list[pos_list.index(key_pos)], end="")
+            print("")
+            row += 1
 
-def swap(keyboard, w1, w2):
-    keyboard[w1], keyboard[w2] = keyboard[w2], keyboard[w1]
+    def get_y(self, w_pos):
+        return (w_pos > 18) + (w_pos > 9)
+
+    def distance(self, key1, key2):
+        key1_y = self.get_y(self.keyboard[key1])
+        key2_y = self.get_y(self.keyboard[key2])
+        key1_x = 4 * (self.keyboard[key1] -
+                      self.rows_num_keys[key1_y]) + self.row_start[key1_y]
+        key2_x = 4 * (self.keyboard[key2] -
+                      self.rows_num_keys[key2_y]) + self.row_start[key2_y]
+        return sqrt((key1_x - key2_x)**2 + (4*key1_y - 4*key2_y)**2)
+
+    def finger_distance(self, key):
+        key_pos = self.keyboard[key]
+        finger = self.finger_group[key_pos]
+        last_finger_key = self.finger_key[finger]
+        self.finger_key[finger] = key
+        if last_finger_key is None:
+            return 0
+        return self.distance(key, last_finger_key)
+
+    def swap(self, w1, w2):
+        self.keyboard[w1], self.keyboard[w2] = self.keyboard[w2], self.keyboard[w1]
 
 
 def main():
-    init_keyboard1 = init_keyboard()
-    display_keyboard(init_keyboard1)
-    print(distance(init_keyboard1, 'A', 'J'))
-    swap(init_keyboard1, 'Q', 'J')
-    display_keyboard(init_keyboard1)
-    print(distance(init_keyboard1, 'A', 'J'))
+    keyboard1 = Keyboard()
+    keyboard1.display_keyboard()
 
 
 if __name__ == "__main__":
--- code/main.py
+++ code/main.py
@@ -1,19 +1,23 @@
 # encoding: utf-8
-import json
+import json,os
 from random import choice
-import displaykeyboard
+import random
+from displaykeyboard import Keyboard
 
-last_changjie = ""
+
 pattern = r'[\t ，。,.:：;；!！?？—\-「」『』【】《》〈〉〔〕〖〗〘〙〚〛〝〞〟〰‥…‧﹏﹑﹔﹖﹪﹫？｡。\\/:*?"<>|\(\)─（）／＊、]★'
+
+
 def chinese_to_unicode(char):
     return f'{ord(char):04x}'
+
 
 def read_json(fileName):
     with open(rf"{fileName}", 'r', encoding='utf-8-sig') as file:
         return json.load(file)
 
 
-def check_total_distance(chinese_str):
+""" def check_total_distance(chinese_str):
     global last_changjie
     total_distance = 0
     for char in chinese_str:
@@ -34,25 +38,56 @@ def check_total_distance(chinese_str):
                 displaykeyboard.keyboard, current_key_chanjie[key - 1],
                 current_key_chanjie[key])
         last_changjie = current_key_chanjie[-1]
+    return total_distance """
+
+
+def check_total_distance(kb: Keyboard, chinese_str):
+    total_distance = 0
+    for char in chinese_str:
+        if char in pattern:
+            continue
+        elif char == "\n":
+            continue
+        try:
+            current_key_chanjie = choice(unicode2cangjie[chinese_to_unicode(char)])
+        except:
+            #print(f"Error: {char} not found in unicode2cangjie")
+            continue
+
+        for key in current_key_chanjie:
+            total_distance += kb.finger_distance(key)
     return total_distance
 
-def test():
-    chinese_str = "儘管如此我還是毫不猶豫地鑽過最後一張網雖然有股想要回頭的衝動"
-    keyboard = displaykeyboard.init_keyboard()
-    displaykeyboard.display_keyboard(keyboard)
-    print(f"Total distance: {check_total_distance(chinese_str)}")
+def random_text(length:int=5000):
+    text_path = r"dataset\text"
+    all_text_files = os.listdir(text_path)
+    text_file = os.path.join(text_path,choice(all_text_files))
+    with open(text_file, 'r', encoding='utf-8') as file:
+        text = file.readlines()
+        total_lines = len(text)
+        start_line = choice(range(total_lines - length))
+        selected_lines = text[start_line:start_line+length]
+        selected_text = ''.join(selected_lines)
+    return selected_text
 
-    displaykeyboard.swap(keyboard, 'Q', 'J')
-    displaykeyboard.swap(keyboard, 'A', 'K')
-    displaykeyboard.swap(keyboard, 'R', 'B')
-    displaykeyboard.display_keyboard(keyboard)
-    print(f"Total distance: {check_total_distance(chinese_str)}")
+def test():
+    chinese_str = random_text()
+    kb = Keyboard()
+    kb.display_keyboard()
+    print(f"Total distance: {check_total_distance(kb, chinese_str)}")
+    kb = Keyboard()
+    kb.swap('Q', 'J')
+    kb.swap('A', 'K')
+    kb.swap('R', 'B')
+    kb.display_keyboard()
+    print(f"Total distance: {check_total_distance(kb, chinese_str)}")
 
 
 def main():
     global unicode2cangjie
     unicode2cangjie = read_json(r"dataset\cangjie\unicode2cangjie.json")
     test()
+
 
 if __name__ == "__main__":
     main()
